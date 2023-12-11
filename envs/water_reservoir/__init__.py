8c7644a0bfff3458c900b6a0db2e5bfcfd31e314
Anna Nickelson <aanickelson@gmail.com>
2023-12-11 15:32:16 -0500

    Adding custom water environment. Update sar_wrapper.py to replace nan with 0's for reward.

--- envs/water_reservoir/__init__.py
+++ envs/water_reservoir/__init__.py
@@ -2,3 +2,4 @@ from gymnasium.envs.registration import register
 
 
 register(id="water-reservoir-v0", entry_point="mo_gymnasium.envs.water_reservoir.dam_env:DamEnv")
+register(id="water-reservoir-new-rw-v0", entry_point="mo_gymnasium.envs.water_reservoir.dam_env_new_rw:DamEnv")
